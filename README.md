7d8a062eb7ec8fc308ea96d6423eb9c6cf571b05
scottyc1000 <scottyc1000@gmail.com>
2014-10-16 17:17:15 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+#SelectSubmit()
+
+The SelectSubmit plugin will submit the form a select element is part of when its value is changed.
+
+##Usage
+
+	$('.select-submit').selectSubmit();
+
+
+##Methods
+
+###Destroy
+
+Use this method to remove the instance.
+
+	$('.select-submit').selectSubmit('destroy');
+
+###Create
+
+Use this method to reinitialize the instance after it has been removed.
+
+	$('.select-submit').selectSubmit('create');
+
---
+++ select-submit.js
@@ -0,0 +1,74 @@
+/*
+*
+* Project: SelectSubmit
+* URL: http://www.carmichaelize.co.uk
+* Author: Scott Carmichael
+* Version: 1.0
+*
+* Copyright (c) 2014 Scott Carmichael
+* Licensed under MIT (http://www.opensource.org/licenses/mit-license.php)
+*
+*/
+
+;(function ($, window, document, undefined) {
+
+    $.fn.selectSubmit = function(select){
+
+        function plugin(select){
+
+            //Get Elements
+            this.$select = select;
+            this.$form = this.$select.parents('form');
+
+            //Methods
+            this.actions(this);
+
+            //Init
+            this.create();
+
+        };
+
+        plugin.prototype.actions = function(self){
+
+            //Submit
+            this.submit = function(){
+                if( self.$form.length > 0 ){
+                    self.$form.submit();
+                }
+            }
+
+            //Create
+            this.create = function(){
+                self.$select.on('change', self.submit);
+            }
+
+            //Destroy
+            this.destroy = function(){
+                self.$select.off('change', self.submit);
+            }
+
+        }
+
+        this.each(function(){
+            if( $(this).is('select') ){
+                if (typeof select === "string" && ( select == 'create' || select == 'destroy' ) ){
+
+                    //Get Plugin Instance
+                    $this = $(this).data('plugin_selectSubmit');
+                    //Call Method
+                    $this[select].call($this);
+
+                } else if ( !$(this).data('plugin_selectSubmit') ) {
+
+                    $(this).data('plugin_selectSubmit', new plugin($(this)) );
+
+                }
+            }
+        });
+
+        //Preserve Chaining
+        return this;
+
+    }
+
+})(jQuery, window, document);
